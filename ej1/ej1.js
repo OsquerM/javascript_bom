792bb151559b6b6dce2b33508d729adcb99f9d00
Oscar <obenmar793@g.educaand>
2025-12-05 12:17:31 +0100

    ej1 hecho

---
+++ ej1/ej1.js
@@ -0,0 +1,6 @@
+function crearVentana() {
+    miVentana = window.open("", "", "width=1280,height=720");
+    miVentana.document.write("<h1>Mi ventana 1280x720</h1>");
+}
+
+document.getElementById("crearVentana").addEventListener("click", crearVentana);
